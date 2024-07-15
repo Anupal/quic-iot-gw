4a9a41fcece7b3e8c43fec1747c0903c2aa51faa
Anupal Mishra <anupal.pm@gmail.com>
2024-07-15 01:18:26 +0100

    logic for CONNECT and REGISTER messages

--- mqtt_sn/gateway.py
+++ mqtt_sn/gateway.py
@@ -1,10 +1,39 @@
 import logging
 import asyncio
 import asyncio_dgram
-from mqtt_sn.message import MQTTSNPacketDecoder, MQTTSNPacketEncoder
+from mqtt_sn.message import MQTTSNPacketDecoder, MQTTSNPacketEncoder, MessageType, ReturnCode
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+class RegisteredTopics:
+    def __init__(self):
+        self.clients = {}
+        self.clients_rev = {}
+        self._topic_id_tracker = {}
+
+    def add_topic(self, client_id, topic_name):
+        if client_id not in self.clients:
+            topic_id = self._next_topic_id(client_id)
+            self.clients[client_id] = {topic_name: topic_id}
+            self.clients_rev[client_id] = {topic_id: topic_name}
+            return topic_id
+        elif topic_name in self.clients[client_id]:
+            return self.clients[client_id][topic_name]
+        else:
+            topic_id = self._next_topic_id(client_id)
+            self.clients[client_id][topic_name] = topic_id
+            self.clients_rev[client_id][topic_id] = topic_name
+            return topic_id
+
+    def topic_id_to_name(self, client_id, topic_id):
+        return self.clients_rev.get(client_id, {}).get(topic_id, None)
+
+    def _next_topic_id(self, client_id):
+        topic_id = self._topic_id_tracker.get(client_id, 1)
+        self._topic_id_tracker[client_id] = topic_id + 1
+        return topic_id
 
 
 class Gateway:
@@ -15,6 +44,8 @@ class Gateway:
         self.mqtt_tasks = set()
         self.decoder = MQTTSNPacketDecoder()
         self.encoder = MQTTSNPacketEncoder()
+        self.mqtt_sn_clients = {}
+        self.registered_topics = RegisteredTopics()
 
     async def run(self):
         logger.info("Starting MQTT gateway server on {}:{}".format(self.host, self.port))
@@ -58,19 +89,60 @@ class Gateway:
             )
             await udp_stream.send(data, remote_addr)
 
-    async def message_handler(self):
+    async def message_dispatcher(self):
         logger.info("Message handler started")
         while True:
             data, remote_addr = await self.dgram_read_queue.get()
             logger.info(f"Message Handler:  {data}, {remote_addr}")
             # decode the message
             decoded_message = self.decoder.decode(data)
+
+            if not decoded_message:
+                # todo: handle unsupported/malformed packets
+                ...
             logger.info(f"Decoded message: {decoded_message}")
 
-            reencoded_message = self.encoder.encode(**decoded_message)
+            if decoded_message["type"] == MessageType.CONNECT:
+                self.mqtt_tasks.add(
+                    asyncio.create_task(self.connect_handler(decoded_message, remote_addr))
+                )
 
-            # echo the message back
-            await self.dgram_write_queue.put((reencoded_message, remote_addr))
+            elif decoded_message["type"] == MessageType.REGISTER:
+                if remote_addr in self.mqtt_sn_clients:
+                    self.mqtt_tasks.add(
+                        asyncio.create_task(self.register_handler(decoded_message, self.mqtt_sn_clients[remote_addr]["client_id"], remote_addr))
+                    )
+                else:
+                    logger.error(f"Unknown client '{remote_addr}'")
+                    logger.info(f"Sending DISCONNECT message to {remote_addr}")
+                    await self.dgram_write_queue.put((self.encoder.encode(
+                        type=MessageType.DISCONNECT
+                    ), remote_addr))
+
+    async def connect_handler(self, message, remote_addr):
+        logger.info(f"Add MQTT-SN client '{remote_addr}' -> '{message['client_id']}'")
+        self.mqtt_sn_clients[remote_addr] = {
+            "client_id": message["client_id"], "duration": message["duration"]
+        }
+        connack_message = self.encoder.encode(
+            type=MessageType.CONNACK,
+            return_code=ReturnCode.ACCEPTED
+        )
+        logger.info(f"Sending CONNACK message: {connack_message} to {remote_addr}")
+        await self.dgram_write_queue.put((connack_message , remote_addr))
+
+    async def register_handler(self, message, client_id, remote_addr):
+        topic_name = message["topic_name"]
+        topic_id = self.registered_topics.add_topic(client_id, topic_name)
+        logger.info(f"Registered MQTT-SN client '{remote_addr}' -> '{client_id}' with topic name '{topic_name}' -> id '{topic_id}'")
+        regack_message = self.encoder.encode(
+            type=MessageType.REGACK,
+            topic_id=topic_id,
+            msg_id=message["msg_id"],
+            return_code=ReturnCode.ACCEPTED
+        )
+        logger.info(f"Sending REGACK message: {regack_message} to {remote_addr}")
+        await self.dgram_write_queue.put((regack_message, remote_addr))
 
     async def stop(self):
         self.reader_task.cancel()
