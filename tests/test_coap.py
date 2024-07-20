f4fbf77afe6c2f9f9517e77736e6a7a668ed3f5e
Anupal Mishra <anupal.pm@gmail.com>
2024-07-20 23:36:45 +0100

    CoAP scale tests - protocol.shutdown() for cleanup

--- tests/test_coap.py
+++ tests/test_coap.py
@@ -16,7 +16,7 @@ PROXY_URI = 'coap://localhost:5683/proxy'
 NUM_CLIENTS = 500
 REQUESTS = 50
 MESSAGE_SIZE_RANGE = (50, 500)  # bytes
-STARTING_PORT = 34201
+STARTING_PORT = 30001
 
 
 def generate_bytes(n):
@@ -31,23 +31,23 @@ class CoAPResource(resource.Resource):
         self.index = index
 
     async def render_get(self, request):
-        # logger.info(f"Received GET request: {request}")
+        logger.info(f"Received GET request: {request}")
         payload = f"Hello, {self.index} ".encode() + generate_bytes(random.randint(*MESSAGE_SIZE_RANGE))
         response = aiocoap.Message(
             payload=payload,
             code=aiocoap.CONTENT
         )
-        # logger.info(f"Sending response: {repr(payload)} | {response}")
+        logger.info(f"Sending response: {repr(payload)} | {response}")
         return response
 
     async def render_post(self, request):
-        # logger.info(f"Received POST request: {request}")
+        logger.info(f"Received POST request: {request}")
         payload = f"Hello, {self.index} ".encode() + request.payload
         response = aiocoap.Message(
             payload=payload,
             code=aiocoap.CHANGED
         )
-        # logger.info(f"Sending response: {repr(payload)} | {response}")
+        logger.info(f"Sending response: {repr(payload)} | {response}")
         return response
 
 
@@ -57,7 +57,7 @@ async def coap_server(num_clients):
     for i in range(1, num_clients + 1):
         root.add_resource([f"hello{i}"], CoAPResource(i))
 
-    # logger.info(f"root={root.get_resources_as_linkheader()}")
+    logger.info(f"root={root.get_resources_as_linkheader()}")
 
     logger.info("Starting CoAP Backend Server")
     await aiocoap.Context.create_server_context(root, bind=("0.0.0.0", 5684))
@@ -80,32 +80,32 @@ async def coap_client(index):
         get_post = random.randint(0, 1)
         time.sleep(random.random() + random.random())
         if get_post == 0:
-            # logger.info(f"Sending GET request through proxy {PROXY_URI} to {resource_uri}")
+            logger.info(f"Sending GET request through proxy {PROXY_URI} to {resource_uri}")
             request = aiocoap.Message(code=aiocoap.GET, uri=PROXY_URI)
             request.opt.proxy_uri = resource_uri
             try:
                 response = await protocol.request(request).response
-                # logger.info(
-                #     f"Received GET response: code='{response.code}' payload='{response.payload.decode()}'")
+                logger.info(
+                    f"Received GET response: code='{response.code}' payload='{response.payload.decode()}'")
                 if response.code == aiocoap.CONTENT and f"Hello, {index} " in response.payload.decode():
                     success += 1
             except Exception as e:
                 logger.error("Error sending GET request", exc_info=e)
         else:
-            # logger.info(f"Sending POST request through proxy {PROXY_URI} to {resource_uri}")
+            logger.info(f"Sending POST request through proxy {PROXY_URI} to {resource_uri}")
             request = aiocoap.Message(code=aiocoap.POST, uri=PROXY_URI,
                                       payload=generate_bytes(random.randint(*MESSAGE_SIZE_RANGE))
                                       )
             request.opt.proxy_uri = resource_uri
             try:
                 response = await protocol.request(request).response
-                # logger.info(
-                #     f"Received POST response: code='{response.code}' payload='{response.payload.decode()}'")
+                logger.info(
+                    f"Received POST response: code='{response.code}' payload='{response.payload.decode()}'")
                 if response.code == aiocoap.CHANGED and f"Hello, {index} " in response.payload.decode():
                     success += 1
             except Exception as e:
                 logger.error("Error sending POST request", exc_info=e)
-
+    await protocol.shutdown()
     return success
 
 
@@ -132,7 +132,7 @@ def main():
     while results.qsize() != NUM_CLIENTS: ...
 
     table = []
-    logger.info("Retrieving results:")
+    logger.warning("Retrieving results:")
     for i in range(NUM_CLIENTS):
         table.append(results.get())
 
@@ -142,7 +142,7 @@ def main():
 
     table.sort(key=lambda x: x[0])
     for index, result in table:
-        logger.info(f"| {index:<5} | {result:<5} |")
+        logger.warning(f"| {index:<5} | {result:<5} |")
 
 
 if __name__ == "__main__":
